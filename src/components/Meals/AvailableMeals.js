486635c969c5a30f5974367508335bf716499f61
cd-x <whowantsthisemailid@gmail.com>
2022-09-17 15:12:31 +0530

    added loading and error handling

--- src/components/Meals/AvailableMeals.js
+++ src/components/Meals/AvailableMeals.js
@@ -5,18 +5,46 @@ import MealItem from "./MealItem/MealItem";
 
 const AvailableMeals = () => {
   const [dummyMeals, setDummyMeals] = useState([]);
-
+  const [isLoading, setIsLoading] = useState(false);
+  const [isError, setIsError] = useState("");
   useEffect(() => {
+    setIsLoading(true);
     const fetchData = async () => {
       const mealsResponse = await fetch(
-        "https://react-http-ee4c9-default-rtdb.firebaseio.com/meals.json"
+        "https://react-http-ee4c9-default-rtdb.firebaseio.com/meals.jso"
       );
+
+      if (!mealsResponse.ok) {
+        throw new Error("Failed to load meals !");
+      }
+
       const mealsData = await mealsResponse.json();
       setDummyMeals(Object.values(mealsData));
+      setIsLoading(false);
     };
 
-    fetchData();
+    fetchData().catch((error) => {
+      setIsLoading(false);
+      setIsError(error.message);
+    });
   }, []);
+
+  if (isLoading) {
+    return (
+      <section className={classes.MealsLoading}>
+        <p>Loading...</p>
+      </section>
+    );
+  }
+
+  if (isError) {
+    return (
+      <section className={classes.MealsError}>
+        <p>{isError}</p>
+      </section>
+    );
+  }
+
   const mealsList = dummyMeals.map((meal) => (
     <li key={meal.id.toString()}>
       <MealItem
